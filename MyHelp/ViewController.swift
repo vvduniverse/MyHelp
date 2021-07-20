c91fce5732c79e8ed49404db383685bc40f9e6c0
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-07-20 14:18:03 +0300

    Initial Commit

---
+++ MyHelp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHelp
+//
+//  Created by Vahtee Boo on 20.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
