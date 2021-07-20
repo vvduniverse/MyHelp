ac871d7605e45b00b30346fa713d04f261876e1f
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-07-20 14:26:05 +0300

    20210720

---
+++ MyHelp/EugeniaRecommendations.swift
@@ -0,0 +1,12 @@
+//
+//  EugeniaRecommendations.swift
+//  MyHelp
+//
+//  Created by Vahtee Boo on 20.07.2021.
+//
+
+import Foundation
+
+//  Проверить, есть ли не используемые аутлеты/экшены
+//  Проверить, есть ли публичные свойста (ненужные): все должны быть приватными
+//  Соответствует ли код структуре (https://debash.medium.com/%D0%BE%D1%84%D0%BE%D1%80%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B8-%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%82%D1%83%D1%80%D0%B0-%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B0-3dc2f150c051)
