47a29a6a956ca179b68a389ec9cee21142376b5a
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-07-20 15:16:41 +0300

    20210720.03

--- MyHelp/EugeniaRecommendations.swift
+++ MyHelp/EugeniaRecommendations.swift
@@ -9,6 +9,8 @@ import Foundation
 
 /*
  Разработка:
+ Называть проект с БольшойБуквыВСтилеКемелКейс
+ Начинать дизайн с самого маленького экрана (SE 1st)
  Функции называть глаголами
  Названия вьюконтролеров - глаголами с большой буквы
 
@@ -16,4 +18,17 @@ import Foundation
   Проверить, есть ли не используемые аутлеты/экшены
   Проверить, есть ли публичные свойста (ненужные): все должны быть приватными
   Соответствует ли код структуре (https://debash.medium.com/%D0%BE%D1%84%D0%BE%D1%80%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B8-%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%82%D1%83%D1%80%D0%B0-%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B0-3dc2f150c051)
+ 
+ // MARK: - IB Outlets
+ // MARK: - Public properties
+ // MARK: - Private properties
+ // MARK: - Initializers
+ // MARK: - Override Methods
+ // MARK: - IB Actions
+ // MARK: - Public Methods
+ // MARK: - Private Methods
+ 
+ 
+ 
+ 
 */
---
+++ MyHelp/UserShortKeys.swift
@@ -0,0 +1,15 @@
+//
+//  UserShortKeys.swift
+//  MyHelp
+//
+//  Created by Vahtee Boo on 20.07.2021.
+//
+
+import Foundation
+/*
+ Ctrl + I - автовыравнивание кода
+ Cmnd + Option + [] - передвижение строк
+ Cmnd + K -  показ/скрытие клавиатуры
+ Cmnd + = - автовыравнивание элементов на сториборде
+ 
+ */
